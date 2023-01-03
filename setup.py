6c00035a3278f865a2ce5c09a333153a965c5ebd
a-chmielewski <chmielaq@gmail.com>
2023-01-03 14:38:47 +0100

    data preparing

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Small exploratory data analysis project of crimes statistics gathered by Polish police',
+    author='achmielewski',
+    license='',
+)
